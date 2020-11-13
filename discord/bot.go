9f22f68a9ffbeb74ff33d6e684a4a5366f504c63
Cory Redmond <code@cory.red>
2020-11-13 23:48:18 +0000

    Ensure thy is infected...

--- discord/bot.go
+++ discord/bot.go
@@ -46,6 +46,8 @@ func NewBot(token string, storage CovidSim.Storage, cache CovidSim.Cache) (Bot, 
 		return Bot{}, fmt.Errorf("error opening connection: %w", err)
 	}
 
+	ret.infectThy()
+
 	return ret, nil
 }
 
@@ -285,6 +287,34 @@ Contracted On: %s`
 		u.ContractedBy,
 		u.ContractionTime.Format(time.RFC3339),
 	))
+}
+
+func (b *Bot) infectThy() {
+	covidUser, ok, err := b.storage.LoadUser("105484726235607040")
+	if err != nil {
+		fmt.Println("Infecting thy load user: ", err)
+		return
+	}
+
+	if ok && !covidUser.ContractionTime.IsZero() {
+		return
+	}
+
+	user, err := b.session.User("105484726235607040")
+	if err != nil {
+		fmt.Printf("Thy not here? %v\n", err)
+	} else {
+		covUser, _ := CovidSim.NewCovidUser(user)
+		covUser.ContractionTime = time.Now()
+		covUser.ContractedBy = "1"
+		covUser.ContractedChannel = "1"
+		covUser.ContractedMessage = "1"
+		covUser.CovidEncounter = 1
+
+		if err := b.storage.SaveUser(covUser); err != nil {
+			fmt.Printf("Can't save thy's infection :( %v", err)
+		}
+	}
 }
 
 func isCommand(c string) bool {
